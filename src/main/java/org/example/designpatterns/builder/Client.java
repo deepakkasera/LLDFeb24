1ebb7cb403d35abd04ca4947181c3ecbd568507b
deepakkasera <deepak.kasera143.dk@gmail.com>
2024-03-18 23:10:30 +0530

    Complete Builder class

--- src/main/java/org/example/designpatterns/builder/Client.java
+++ src/main/java/org/example/designpatterns/builder/Client.java
@@ -2,15 +2,26 @@ package org.example.designpatterns.builder;
 
 public class Client {
     public static void main(String[] args) {
-        Builder builder = new Builder();
-        builder.setName("Zahidul");
-        builder.setAge(25);
-        builder.setBatch("MWF Evening");
-        builder.setPsp(90.0);
-        builder.setGradYear(2022);
-        builder.setPhoneNumber("123456789");
+        //Builder builder = new Builder();
+//        Builder builder = Student.getBuilder();
+//        builder.setName("Zahidul");
+//        builder.setAge(25);
+//        builder.setBatch("MWF Evening");
+//        builder.setGradYear(2022);
+//        builder.setPhoneNumber("123456789");
 
-        Student student = new Student(builder);
+        //Student student = new Student(builder);
+
+//        Student student = builder.build();
+
+        //Student student1 = new Student(null);
+
+        Student student = Student.getBuilder()
+                        .setName("Zahidul")
+                        .setAge(25)
+                        .setGradYear(2024)
+                        .setPhoneNumber("1234")
+                        .build();
 
         System.out.println("DEBUG");
     }
--- src/main/java/org/example/designpatterns/builder/Student.java
+++ src/main/java/org/example/designpatterns/builder/Student.java
@@ -9,20 +9,101 @@ public class Student {
     int gradYear;
     String phoneNumber;
 
-    public Student(Builder builder) {
-        //Validations start
-        if (builder.getAge() < 20) {
-            throw new RuntimeException("Age shouldn't be less than 20");
-        }
-        if (builder.getGradYear() > 2023) {
-            throw new RuntimeException("Grad year shouldn't be more than 2023");
-        }
-        //Validations end
+    public static Builder getBuilder() {
+        return new Builder();
+    }
 
+    private Student(Builder builder) {
         this.name = builder.getName();
         this.age = builder.getAge();
         this.gradYear = builder.getGradYear();
         this.psp = builder.getPsp();
         ///
+    }
+
+    public static class Builder { // Inner class.
+        String name;
+        int age;
+        String univName;
+        double psp;
+        String batch;
+        int gradYear;
+        String phoneNumber;
+
+        public String getName() {
+            return name;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public String getUnivName() {
+            return univName;
+        }
+
+        public Builder setUnivName(String univName) {
+            this.univName = univName;
+            return this;
+        }
+
+        public double getPsp() {
+            return psp;
+        }
+
+        public Builder setPsp(double psp) {
+            this.psp = psp;
+            return this;
+        }
+
+        public String getBatch() {
+            return batch;
+        }
+
+        public Builder setBatch(String batch) {
+            this.batch = batch;
+            return this;
+        }
+
+        public int getGradYear() {
+            return gradYear;
+        }
+
+        public Builder setGradYear(int gradYear) {
+            this.gradYear = gradYear;
+            return this;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public Builder setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Student build() {
+            //Validations start
+            if (this.getAge() < 20) {
+                throw new RuntimeException("Age shouldn't be less than 20");
+            }
+            if (this.getGradYear() > 2023) {
+                throw new RuntimeException("Grad year shouldn't be more than 2023");
+            }
+            //Validations end
+
+            return new Student(this);
+        }
     }
 }
